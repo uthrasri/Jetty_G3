6464078295578e799dc59ea72422d11d4a3d5802
uthrasri <145666390+uthrasri@users.noreply.github.com>
2023-11-02 02:05:15 +0530

    Update SslConnection.java

--- jetty-io/src/main/java/org/eclipse/jetty/io/ssl/SslConnection.java
+++ jetty-io/src/main/java/org/eclipse/jetty/io/ssl/SslConnection.java
@@ -729,8 +729,15 @@ public class SslConnection extends AbstractConnection implements Connection.Upgr
                                     return filled = -1;
 
                                 case BUFFER_UNDERFLOW:
+                                    if (BufferUtil.space(_encryptedInput) == 0)
+                                    {
+                                        BufferUtil.clear(_encryptedInput);
+                                        throw new SSLHandshakeException("Encrypted buffer max length exceeded");
+                                    }
+    
                                     if (netFilled > 0)
                                         continue; // try filling some more
+                                    
                                     _underflown = true;
                                     if (netFilled < 0 && _sslEngine.getUseClientMode())
                                     {
@@ -739,7 +746,7 @@ public class SslConnection extends AbstractConnection implements Connection.Upgr
                                         {
                                             Throwable handshakeFailure = new SSLHandshakeException("Abruptly closed by peer");
                                             if (closeFailure != null)
-                                                handshakeFailure.initCause(closeFailure);
+                                                handshakeFailure.addSuppressed(closeFailure);
                                             throw handshakeFailure;
                                         }
                                         return filled = -1;
